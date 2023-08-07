70b9afa54bed13e6cbf12f7ee2e381ca7321aa42
Rocio-Peralta <1rocioperalta@gmail.com>
2023-08-07 18:50:35 +1200

    fetch the Api and add a button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export function App () {
-  return <h1> HOLA</h1>
+const FACT_ENDPOINT = 'https://catfact.ninja/fact'
+export function App() {
+  const [fact, setFact] = useState()
+  useEffect(() => {
+    fetch(FACT_ENDPOINT)
+      .then((res) => res.json())
+      .then((data) => setFact(data.fact))
+  }, [])
+
+  function handleClick() {
+   
+      fetch(FACT_ENDPOINT)
+        .then((res) => res.json())
+        .then((data) => setFact(data.fact))
+    
+  }
+
+  return (
+    <main>
+      <h1> {fact}</h1>
+      <button onClick={handleClick}>Again?</button>
+    </main>
+  )
 }
